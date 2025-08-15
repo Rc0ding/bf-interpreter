050aa6f6775dfee5a72f0546aef2bcd8808487ec
Remi Sergi <remi.sergi@epfl.ch>
2025-08-16 01:11:06 +0200

    scrape function that uses regex to scrape all the bf instruction characters, and a test.bf file to test my interpreter

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
-def main():
-    print("Hello from bf-interpreter!")
+import re, sys
+from typing import Any
 
+def main():
+
+    if len(sys.argv) < 2:
+        print("Usage: python MyFile.py <file.bf>")
+        sys.exit(1)
+
+    filename = sys.argv[1]
+
+    with open(filename, "r", encoding="utf-8") as f:
+        source_code = f.read()
+
+    instructions = scrape(source_code)
+    print(instructions)
+
+    #tape: list[int]= 30000*[0]
+    #dataPointer: int=0
+    #print(tape)
+
+
+
+def output(number:int)->str:
+    return chr(number%256)
+
+
+def add(tape:list[int],dP:int)->int:
+    tape[dP]+=1
+    return 0
+
+def scrape(code: str) -> list[Any]:
+    return re.findall(r"[+\-<>.,\[\]]", code)
 
 if __name__ == "__main__":
     main()
